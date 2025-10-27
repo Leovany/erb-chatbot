daf76966dc8e681371afda588b11eab66e67d0f4
Leovany <348083098@qq.com>
2025-10-27 15:17:52 +0800

    增加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api"); // 所有路由添加 /api 前缀
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Chatbot API')
+    .setDescription('The chatbot API description')
+    .setVersion('1.0')
+    .addTag('Chatbot')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
+  await app.listen(process.env.PORT ?? 3000);  
+
   console.log("应用运行在: http://localhost:3000");
 }
 bootstrap();
