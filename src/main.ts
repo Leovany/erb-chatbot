6f588d74038b949fbea1474c71ba0f0126c39da2
Leovany <348083098@qq.com>
2025-10-27 15:24:07 +0800

    修改swgger标题

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Chatbot API')
+    .setTitle('CT289DS006-Chatbot API')
     .setDescription('The chatbot API description')
     .setVersion('1.0')
     .addTag('Chatbot')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  
+
   await app.listen(process.env.PORT ?? 3000);  
 
   console.log("应用运行在: http://localhost:3000");
